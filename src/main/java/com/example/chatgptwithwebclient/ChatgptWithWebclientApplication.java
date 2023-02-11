d6a0eed91452e89a12fa65ebe9d74ad19dca0732
const-k <kss07120@gmail.com>
2023-02-11 15:10:24 +0900

    initial commit, create springboot project

---
+++ src/main/java/com/example/chatgptwithwebclient/ChatgptWithWebclientApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatgptwithwebclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptWithWebclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatgptWithWebclientApplication.class, args);
+    }
+
+}
